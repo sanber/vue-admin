ee2d095ada26f8468fe82f28cf0deffd29b45e63
这里换上你的用户名 <18974091132@163.com>
2020-10-16 16:22:50 +0800

    构建项目、配置全局样式、路由、elementui

---
+++ src/views/Login/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="login">
+    登录页
+    <el-button round>圆角按钮</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login"
+};
+</script>
+
+<style lang="scss" scoped>
+#login {
+  height: 100vh;
+  background: #344a5f;
+}
+</style>
