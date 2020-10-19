0c24a7cd0b1a6f41847584d303baa60b9b73f69f
sanber <651973990@qq.com>
2020-10-19 22:10:05 +0800

    提交了登录页tab切换功能

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,13 +1,40 @@
 <template>
   <div id="login">
-    登录页
-    <el-button round>圆角按钮</el-button>
+    <ul class="menuTab">
+      <li
+        :class="{ activeStyle: item.activeName }"
+        v-for="(item, index) in menuList"
+        :key="index"
+        @click="changeTab(item)"
+      >
+        {{ item.text }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data() {
+    return {
+      menuList: [
+        { text: "登录", activeName: true },
+        { text: "注册", activeName: false }
+      ],
+      isActive: true
+    };
+  },
+  methods: {
+    changeTab(item) {
+      this.menuList.forEach(ele => {
+        ele.activeName = false;
+        // console.log(ele);
+      });
+      item.activeName = true;
+      // console.log(this.menuList, item);
+    }
+  }
 };
 </script>
 
@@ -15,5 +42,22 @@ export default {
 #login {
   height: 100vh;
   background: #344a5f;
+  .menuTab {
+    width: 330px;
+    margin: 0 auto;
+    overflow: hidden;
+    li {
+      width: 50%;
+      height: 60px;
+      line-height: 60px;
+      float: left;
+      text-align: center;
+      color: #ffffff;
+      cursor: pointer;
+    }
+    .activeStyle {
+      border-bottom: 3px #409eff solid;
+    }
+  }
 }
 </style>
